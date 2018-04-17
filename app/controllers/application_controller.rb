cbbb80a8b9ea68fb3996ee9344aace969206b84e
marco ranieri <mr.marcoranieri@gmail.com>
2018-04-17 10:56:43 +0200

    add controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception # XSS attack
+
 #white-list approach: every routes is close (add exception in each controller)
   before_action :authenticate_user!
 
 end
+
