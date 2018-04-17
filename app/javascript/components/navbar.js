306d15380b9b87381ea04cb585d7ebad405a7b08
marco ranieri <mr.marcoranieri@gmail.com>
2018-04-17 22:56:41 +0200

    nice cards on landing page

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= window.innerHeight) {
+    if (window.scrollY >= 50) {
       navbar.classList.add('navbar-wagon-white');
     } else {
       navbar.classList.remove('navbar-wagon-white');
