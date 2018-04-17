0529669d5cdf2e6de41cdaf6372e084f4bf8d0b0
Cali Arboleda <caliarboleda@yahoo.com>
2018-04-17 17:17:41 +0200

    banner and navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :bookings
-  has_many :cars
+  has_many :cars, dependent: :destroy
   has_many :rented_cars, through: :bookings, source: :car
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
+
