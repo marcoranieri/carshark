daf918916d6b13d98117cb77ad0597d6ef7d8321
michele saladino <michelelewagon@gmail.com>
2018-04-18 14:05:10 +0200

    add edit profile page

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@
 function initUpdateNavbarOnScroll() {
   const navbar = document.querySelector('.navbar-wagon');
   window.addEventListener('scroll', () => {
-    if (window.scrollY >= 50) {
+    if (window.scrollY >= window.innerHeight) {
       navbar.classList.add('navbar-wagon-white');
     } else {
       navbar.classList.remove('navbar-wagon-white');
