e5dee55362ea91ae9a7f4c2f4976236c760d5998
Cali Arboleda <caliarboleda@yahoo.com>
2018-04-19 16:17:02 +0200

    dashboard, card css refactor, unbreak bootstrap row, partials for cards used in dashboard, general common sense

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cars
+
+  get '/dashboard', to: "dashboard#show"
  end
